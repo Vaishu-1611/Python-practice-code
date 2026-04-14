a5c94fba96b30206ba7cfaa3c2e5a740574be85b
Vaishu-1611 <vmhetre608@gmail.com>
2026-04-14 12:32:27 +0530

    list operations

---
+++ lst.py
@@ -0,0 +1,26 @@
+l = list(map(int, input("Enter the numbers:").split()))
+print("the list is:",l)
+print("the maximum number is:",max(l))
+print("the minimum number is:",min(l))
+l.sort()
+print("the sorted list is:",l)
+print("the maximun number is:",l[-1])
+print("the minimum number is:",l[0])
+print("the second largest number is:",l[-2])
+print("the second smallest number is:",l[1])
+
+print("even numbers in the list are:")
+for i in range(len(l)):
+    if l[i] % 2 == 0:
+        print(l[i])
+print("odd numbers in the list are:")
+for i in range(len(l)):
+    if l[i] % 2 != 0:
+        print(l[i])
+
+sum=0
+for items in l:
+    sum += items
+print("the sum of numbers in list is:",sum)
+
+print("list after removing duplicates is:",list(set(l)))
